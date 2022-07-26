0d8f1ace66a240a1a086087521c2da5496a3f2e9
mayurkalia99 <mayur.k@stackera.io>
2022-07-26 18:29:48 +0530

    creating post

--- components/Stories.js
+++ components/Stories.js
@@ -1,30 +1,29 @@
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 import React from "react";
-import { ScrollView } from "react-native-web";
 
 const users = [
   {
-    name: "Mayur",
+    name: "mayurkalia_",
     image:
       "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
   },
   {
-    name: "Nidi",
+    name: "nidishapatil_",
     image:
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
   },
   {
-    name: "Faiz",
+    name: "Faiz_bbx",
     image:
       "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
   },
   {
-    name: "Vijay",
+    name: "vjaris",
     image:
       "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
   },
   {
-    name: "Sayli",
+    name: "Sayli_lonkar",
     image:
       "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
   },
@@ -32,14 +31,19 @@ const users = [
 
 const Stories = () => {
   return (
-    <View style={{ marginBottom: 10 }}>
-      <ScrollView horizontal showHorizontalScrollIndicator={false}>
+    <View style={styles.container}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
         {users.map((user, index) => {
-          <View style={styles.story}>
-            <Image style={styles.image} source={{ uri: user.image }} />
-            <Text style={styles.userName}>{user.name}</Text>;
-            {console.log(user.name)}
-          </View>;
+          return (
+            <View key={index} style={{ alignItems: "center" }}>
+              <Image style={styles.image} source={{ uri: user.image }} />
+              <Text style={{ color: "white" }}>
+                {user.name.length > 10
+                  ? user.name.slice(0, 9).toLowerCase() + "..."
+                  : user.name.toLowerCase()}{" "}
+              </Text>
+            </View>
+          );
         })}
       </ScrollView>
     </View>
@@ -47,11 +51,21 @@ const Stories = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {},
-  image: {
+  container: {
     height: 100,
-    width: 100,
-    resizeMode: "contain",
+    marginBottom: 10,
+  },
+  image: {
+    height: 70,
+    width: 70,
+    resizeMode: "cover",
+    borderRadius: 50,
+    marginLeft: 6,
+    borderWidth: 3,
+    borderColor: "#ff8501",
+  },
+  story: {
+    height: "100",
   },
   userName: {
     color: "white",
